1f6dcd03b7be00c3dbcfc506d533d163f7aee3fb
shaheer3xx6 <jahangeersaleem.gm@gmail.com>
2026-01-31 22:23:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "Wow Do you love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "Wow, do you love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
